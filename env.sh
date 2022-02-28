e100a437fb29eb383f978d7527873f115ab0dcc0
Luke Zhang <az16408@bc4login2.bc4.acrc.priv>
2022-02-28 15:04:53 +0000

    load latest version of gcc and intel icc

--- env.sh
+++ env.sh
@@ -1,2 +1,9 @@
 # Add any `module load` or `export` commands that your code needs to
 # compile and run to this file.
+# set OMP_NUM_THREADS=28
+# set OMP_NUM_THREADS #This command displays the current setting of the OMP_NUM_THREADS environment variable
+module load languages/gcc/9.3.0
+module load languages/intel/2020-u4
+module load intel/2017.01
+# module load vtune/2017.1.132-GCC-5.4.0-2.26
+# module load tools/valgrind/3.15.0
