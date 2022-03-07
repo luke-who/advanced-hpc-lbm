9300ad8bb6d8657b3cde83eee82ee2e3361c73ed
luke-who <az16408@bristol.ac.uk>
2022-03-07 13:40:37 +0000

    added restrict for t_speed

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -258,7 +258,7 @@ int accelerate_flow(const t_param params, t_speed* cells, int* obstacles)
   return EXIT_SUCCESS;
 }
 
-int propa_rebd_collsn(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles)
+int propa_rebd_collsn(const t_param params, t_speed* restrict cells, t_speed* restrict tmp_cells, int* obstacles)
 {
   const float c_sq = 1.f / 3.f; /* square of speed of sound */
   const float w0 = 4.f / 9.f;  /* weighting factor */
