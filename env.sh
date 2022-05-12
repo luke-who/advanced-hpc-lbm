025f5b7d8740aee1837c0cda97e99fcdfdc7a9a9
luke-who <az16408@bristol.ac.uk>
2022-05-12 23:27:16 +0100

    update `comments`

--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
 # Add any `module load` or `export` commands that your code needs to
 # compile and run to this file.
 
+# /* set OMP env variables */
 export OMP_NUM_THREADS=14
 # set OMP_NUM_THREADS #This command displays the current setting of the OMP_NUM_THREADS environment variable
 # export OMP_SCHEDULE=static #pragma omp parallel for uses static schedule by default, which evenly divides the total loop iterations between all threads
@@ -9,7 +10,10 @@ export OMP_PROC_BIND=close
 # export OMP_PROC_BIND=spread
 export OMP_PLACES=cores
 
+# /* set MPI env variables */
 # export I_MPI_DEBUG=4
+
+# /* load necessary modules */
 # module load languages/gcc/9.3.0
 module load languages/intel/2020-u4 #The Intel compiler is available in the `languages/intel/2020-u4` module. This module puts advixe-cl in your path, so there will be no need to source anything
 # module load intel/2017.01
